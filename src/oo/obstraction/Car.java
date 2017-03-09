2d34865a6cfee8e4210820e6a0dfdf9ab8e6a0d4
YulingChang <aa0932299020@gmail.com>
2017-03-09 23:36:53 +0800

    Update Car.java

--- src/oo/obstraction/Car.java
+++ src/oo/obstraction/Car.java
@@ -1,10 +1,11 @@
 package oo.obstraction;
 
-class Car {
+public class Car {
+	String brand;
+	int cc;
+	String name;
+	String type;
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
