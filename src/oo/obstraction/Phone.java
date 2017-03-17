de4c364880834bd5eb6d4fa500f57f3105a77eeb
YulinngChang <aa0932299020@gmail.com>
2017-03-17 19:02:02 +0800

    practice

--- src/oo/obstraction/Phone.java
+++ src/oo/obstraction/Phone.java
@@ -1,9 +1,17 @@
 package oo.obstraction;
 
 public class Phone {
-  String brand;
-	int price;
+    String brand;
 	String name;
+	int price;
 	String color;
+	
+	public Phone(String brand,String name,int price,String color){
+		this.brand = brand;
+		this.name = name;
+		this.price = price;
+		this.color = color;
+		
 
+}
 }
--- src/oo/obstraction/PhoneTester.java
+++ src/oo/obstraction/PhoneTester.java
@@ -3,7 +3,9 @@ package oo.obstraction;
 public class PhoneTester {
 
 	public static void main(String[] args) {
-		Phone p1 = new Phone();
+		Phone p1 = new Phone("SAMSUNG","GALAXYJ7",8900,"pink");
+		Phone p2 = new Phone("HTC","DESIRE820",10000,"white");
+		Phone p3 = new Phone("APPLE","6S",20000,"gold");
 
 	}
 
